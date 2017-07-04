0d50d12d70a0d8af78919705172f147bf3065840
rgbosque <rgbosque@gmail.com>
2017-07-04 09:37:42 +0800

    Create skeleton of Crime Map App

---
+++ dbhelper.py
@@ -0,0 +1,40 @@
+from config import config
+import psycopg2
+
+
+class DBHelper():
+
+    def connect(self):
+        params = config()
+        conn = psycopg2.connect(**params)
+        return conn
+
+    def get_all_inputs(self):
+        connection = self.connect()
+        try:
+            query = "SELECT description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                return cursor.fetchall()
+        finally:
+            connection.close()
+
+    def add_input(self, data):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes(description) VALUES ('{}');".format(data)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+        finally:
+            connection.close()
+
+    def clear_all(self):
+        connection = self.connect()
+        try:
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+        finally:
+            connection.close()
