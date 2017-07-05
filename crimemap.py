680e2da307a341d1228b88ce4f7da099094dc373
rgbosque <rgbosque@gmail.com>
2017-07-05 21:54:05 +0800

    add validation and sanitization in data entry

--- crimemap.py
+++ crimemap.py
@@ -1,3 +1,6 @@
+import datetime
+import dateparser
+import string
 import json
 from dbhelper import DBHelper
 from flask import Flask, render_template, request
@@ -5,9 +8,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 
 @app.route('/')
-def home():
+def home(error_msg=None):
     # try:
     #     data = DB.get_all_inputs()
     # except Exception as e:
@@ -16,37 +21,42 @@ def home():
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
 
-    return render_template('home.html', crimes=crimes)
-
-
-@app.route('/add', methods=['POST'])
-def add():
-    try:
-        data = request.form.get('userinput')
-        DB.add_input(data)
-    except Exception as e:
-        print e
-    return home()
-
-
-@app.route('/clear')
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print e
-    return home()
+    return render_template('home.html', crimes=crimes, categories=categories,
+                           error_msg=error_msg)
 
 
 @app.route('/submitcrime', methods=['POST'])
 def submitcrime():
     category = request.form.get('category')
-    date = request.form.get('date')
-    latitude = float(request.form.get('latitude'))
-    longitude = float(request.form.get('longitude'))
+    if category not in categories:
+        return home()
+
+    date = format_date(request.form.get('date'))
+    if not date:
+        return home("INVALID DATE! Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get('latitude'))
+        longitude = float(request.form.get('longitude'))
+    except ValueError:
+        return home()
+
     description = request.form.get('description')
+    description = sanitize_string(request.form.get('description'))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 if __name__ == '__main__':
